d7604c67abb3955c958dcbdabe81a64401d0eb3e
amin boutarfi <mr.amineboutarfi@gmail.com>
2023-03-13 13:01:39 +0100

    added initial code

---
+++ README.md
@@ -0,0 +1,18 @@
+# Booking.com Scraper
+
+This is simple scraper that uses Playwright to extract data from Booking.com
+
+This example is made for educational purposese.
+
+This scrapit is easy to customize.
+
+check both Excel & CSV files (hotels_list) to see how final data will look like. 
+
+## To Install:
+- (Optional: create & activate a virtual environment) `virtualenv venv`, then `source venv/bin/activate`
+
+- `pip install -r requirements.txt`
+- `playwright install chromium`
+
+## to Run:
+- `python3 booking_scraper.py` 
---
+++ booking_scraper.py
@@ -0,0 +1,40 @@
+from playwright.sync_api import sync_playwright
+import pandas as pd
+
+
+def main():
+    
+    with sync_playwright() as p:
+        
+        # IMPORTANT: Change dates to future dates, otherwise it won't work
+        checkin_date = '2023-03-23'
+        checkout_date = '2023-03-24'
+        
+        page_url = f'https://www.booking.com/searchresults.en-us.html?checkin={checkin_date}&checkout={checkout_date}&selected_currency=USD&ss=Paris&ssne=Paris&ssne_untouched=Paris&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_type=city&group_adults=1&no_rooms=1&group_children=0&sb_travel_purpose=leisure'
+
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(page_url, timeout=60000)
+                    
+        hotels = page.locator('//div[@data-testid="property-card"]').all()
+        print(f'There are: {len(hotels)} hotels.')
+
+        hotels_list = []
+        for hotel in hotels:
+            hotel_dict = {}
+            hotel_dict['hotel'] = hotel.locator('//div[@data-testid="title"]').inner_text()
+            hotel_dict['price'] = hotel.locator('//span[@data-testid="price-and-discounted-price"]').inner_text()
+            hotel_dict['score'] = hotel.locator('//div[@data-testid="review-score"]/div[1]').inner_text()
+            hotel_dict['avg review'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[1]').inner_text()
+            hotel_dict['reviews count'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[2]').inner_text().split()[0]
+
+            hotels_list.append(hotel_dict)
+        
+        df = pd.DataFrame(hotels_list)
+        df.to_excel('hotels_list.xlsx', index=False) 
+        df.to_csv('hotels_list.csv', index=False) 
+        
+        browser.close()
+            
+if __name__ == '__main__':
+    main()
